23cb79de38b0e152a15119864369915d17c204fd
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-03 16:10:29 +0500

    restart

--- handlers.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Hello!, welcome to bot!'
-    )
